7c39242b76c191ec664fad24006e17ed1c522c92
Artimes22 <artimes.flima@gmail.com>
2023-11-23 19:06:13 -0300

    aulas

---
+++ logica_de_programacao/aula13.py
@@ -0,0 +1,12 @@
+nome = 'Artimes de Lima'
+altura = 1.74
+peso = 62
+imc = peso / (altura * altura)
+
+linha_1 = f'{nome} tem {altura:.2f} de altura'
+linha_2 = f'Pesa {peso} quilos e seu IMC é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
---
+++ logica_de_programacao/aula15.py
@@ -0,0 +1,11 @@
+#nome = input('Qual o seu nome? ')
+
+#print(f'Seu nome é {nome}')
+
+n1 = input('Digite um numero: ')
+n2 = input('Digite outro numero: ')
+
+int_n1 = int(n1)
+int_n2 = int(n2)
+soma = int_n1 + int_n2
+print(f'A soma entre {int_n1} e {int_n2} é igual a {soma}')
