927541ba0626f01401a5c8bf7c136ca288817b5f
Artimes22 <artimes.flima@gmail.com>
2023-12-12 18:46:16 -0300

    aula

--- python_intermediario/aula94.py
+++ python_intermediario/aula94.py
@@ -1,1 +1,10 @@
 # Try, except, else e finally
+try:
+    print('Abrir arquivo')
+    8/0
+except ZeroDivisionError:
+    print('Dividiu zero')   
+else: #Vai executar caso não ocorra erros no código
+    print('Não ocorreu erros')     
+finally: #Sempre será executado mesmo tendo algum erro
+    print('Fechar arquivo')
