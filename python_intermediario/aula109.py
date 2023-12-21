6821aef16596c23e42b4599e265dc3160cff6a25
Artimes22 <artimes.flima@gmail.com>
2023-12-21 06:52:07 -0300

    aulas

--- python_intermediario/aula109.py
+++ python_intermediario/aula109.py
@@ -2,3 +2,27 @@
 # Combinação - Ordem não importa - iterável + tamanho do grupo
 # Permutação - Ordem importa
 # Produto - Ordem importa e repete valores únicos 
+from itertools import combinations, permutations, product
+
+
+def print_iter(iterator):
+    print(*list(iterator), sep='\n')
+    print()
+
+
+pessoas = [
+    'João', 'Joana', 'Luiz', 'Letícia',
+]
+camisetas = [
+    ['preta', 'branca'],
+    ['P', 'M', 'G', 'GG'],
+    ['Masculino', 'Feminino'],
+    ['Algodão', 'Poliéster'],
+]
+# print('Combinações')
+# print_iter(combinations(pessoas, 2))
+# print('Permutações')
+# print_iter(permutations(pessoas, 2))
+print('Produtos')
+print_iter(product(*camisetas))
+
