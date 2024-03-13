d66c3fa99d69c4ff19d405a49146f5e1a3ed3d30
Artimes22 <artimes.flima@gmail.com>
2024-03-13 16:41:18 -0300

    aula

--- introducao_programacao_orientada_objetos/aula162.py
+++ introducao_programacao_orientada_objetos/aula162.py
@@ -6,7 +6,8 @@
 # datetime.fromtimestamp(Unix Timestamp)
 # Para timezones
 
-from datetime import datetime
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 # from pytz import timezone
 
 
@@ -21,5 +22,15 @@ from datetime import datetime
 
 # data = datetime.now(timezone('America/Sao_Paulo'))
 
-data = datetime.now()
-print(data.timestamp())
+# data = datetime.now()
+# print(data.timestamp())
+
+fmt = '%d/%m/%Y %H:%M:%S'
+data_inicio = datetime.strptime('20/04/1987 09:30:30', fmt)
+data_fim = datetime.strptime('12/12/2022 08:20:20', fmt)
+
+delta = timedelta(days=10)
+print(data_fim + delta)
+print(data_fim + relativedelta(seconds=60))
+# delta = data_fim - data_inicio/
+# print(delta.days, delta.seconds, delta.microseconds)
