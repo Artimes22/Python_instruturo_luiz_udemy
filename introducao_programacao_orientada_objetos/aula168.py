55b11889cff94817b9f500f57a5153be66115551
Artimes22 <artimes.flima@gmail.com>
2024-04-04 17:23:54 -0300

    aulas

---
+++ introducao_programacao_orientada_objetos/aula168.py
@@ -0,0 +1,17 @@
+# O módulo 'os' para interação com o sistema
+# O módulo 'os' fornece funções para interagir com o sistema operacional.
+# Por exemplo, o módulo os.path contém funções para trabalhar com caminhos de
+# arquivos e funções os.listdir() pode ser usada para listar os arquivos em um
+# diretório. O método os.system() permite executar comandos do sistema
+# operacional a partir do código Python.
+
+import os
+
+os.system('cls')
+
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
